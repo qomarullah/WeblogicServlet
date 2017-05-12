e01f27b34e7ef587037554f7f6375840450182d6
qomarullah <qomarullah.mail@gmail.com>
2017-05-12 16:13:35 +0700

    initial upload

---
+++ src/Driver.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class Driverp
+ */
+@WebServlet("/")
+public class Driver extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	//private static Logger log = Logger.getLogger(Driver.class);
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Driver() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		response.getWriter().print("Bismillah");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
